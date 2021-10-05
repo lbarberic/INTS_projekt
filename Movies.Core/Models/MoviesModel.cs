d0e922eeaa418f36ba7980319fc05df3a7bdf5bd
Luka Barberić <barberic.luka10@gmail.com>
2021-10-05 09:53:37 +0200

    New project

---
+++ Movies.Core/Models/MoviesModel.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Movies.Core.Model
+{
+#nullable enable
+    [BsonIgnoreExtraElements]
+    public class MoviesModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("title")]
+        public string? Title { get; set; }
+
+        [BsonElement("rated")]
+        public string? Rated { get; set; }
+
+        [BsonElement("runtime")]
+        public int? Runtime { get; set; }
+
+        [BsonElement("year")]
+        public int? Year { get; set; }
+
+        [BsonElement("genres")]
+        public string[]? Genres { get; set; }
+
+        [BsonElement("cast")]
+        public string[]? Cast { get; set; }
+
+        [BsonElement("directors")]
+        public string[]? Directors { get; set; }
+        public double Similarity { get; set; }
+        [BsonElement("poster")]
+        public string? Poster { get; set; }
+    }
+}
---
+++ Movies.Core/MoviesService.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using Movies.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Core
+{
+    public class MoviesService
+    {
+        private MongoClient InitializeClient()
+        {
+            var client = new MongoClient("mongodb://user:pass1234@cluster0-shard-00-00.vr1tq.mongodb.net:27017,cluster0-shard-00-01.vr1tq.mongodb.net:27017,cluster0-shard-00-02.vr1tq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mxqr3g-shard-0&authSource=admin&retryWrites=true&w=majority");
+            return client;
+        }
+
+        public async Task<List<MoviesModel>> GetAllMovies()
+        {
+            var client = InitializeClient();
+            var database = client.GetDatabase("sample_mflix");
+            var collection = database.GetCollection<MoviesModel>("movies");
+            var response = collection.Find(movie => movie.Year >= 2005).ToListAsync();
+
+            return await response;
+        }
+
+
+        public async Task<List<MoviesModel>> GetSpecificMovie(string name)
+        {
+            var client = InitializeClient();
+            var database = client.GetDatabase("sample_mflix");
+            var collection = database.GetCollection<MoviesModel>("movies");
+            var response = collection.Find(movie => movie.Title.ToLower() == name.ToLower()).ToListAsync();
+
+            return await response;
+        }
+    }
+}
