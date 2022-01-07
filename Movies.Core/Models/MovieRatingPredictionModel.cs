d6b1efeebf65537bf690954fdd6420ff57f55704
Luka Barberić <lbarberic@foi.hr>
2022-01-07 20:30:09 +0100

    Added new files

---
+++ Movies.Core/Models/MovieRatingPredictionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Core.Models
+{
+    public class MovieRatingPredictionModel
+    {
+        public float Label;
+        public float Score;
+    }
+}
--- Movies.Core/MoviesService.cs
+++ Movies.Core/MoviesService.cs
@@ -1,38 +1,107 @@
-﻿using MongoDB.Driver;
-using Movies.Core.Model;
+﻿using Microsoft.ML;
+using Microsoft.ML.Trainers;
+using MongoDB.Driver;
+using Movies.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Movies.Core
 {
     public class MoviesService
     {
-        private MongoClient InitializeClient()
+        private static string trainingDataPath = Path.Combine(Environment.CurrentDirectory, @"Data\recommendation-ratings-train.csv");
+        private static string testDataPath = Path.Combine(Environment.CurrentDirectory, @"Data\recommendation-ratings-test.csv");
+        private static string moviesPath = Path.Combine(Environment.CurrentDirectory, @"Data\movie.csv");
+        private static List<MovieModel> All = new List<MovieModel>();
+
+        static void Main(string[] args)
         {
-            var client = new MongoClient("mongodb://user:pass1234@cluster0-shard-00-00.vr1tq.mongodb.net:27017,cluster0-shard-00-01.vr1tq.mongodb.net:27017,cluster0-shard-00-02.vr1tq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mxqr3g-shard-0&authSource=admin&retryWrites=true&w=majority");
-            return client;
+            StreamReader reader = new StreamReader(File.OpenRead(moviesPath));
+
+            reader.ReadLine();
+            int counter = 2;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                var nesta = new MovieModel
+                {
+                    Id = Int32.Parse(values[0]),
+                    Title = values[1]
+                };
+
+                All.Add(nesta);
+            }
+            var mlContext = new MLContext();
+
+
+            var trainingDataView = mlContext.Data.LoadFromTextFile<MovieRatingModel>(trainingDataPath, hasHeader: true, separatorChar: ',');
+            var testDataView = mlContext.Data.LoadFromTextFile<MovieRatingModel>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "movieIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 100000,
+                ApproximationRank = 512,
+                Lambda = 0.81,
+                LearningRate = 0.000196
+            };
+
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey(
+                    inputColumnName: "userId",
+                    outputColumnName: "userIdEncoded")
+                .Append(mlContext.Transforms.Conversion.MapValueToKey(
+                    inputColumnName: "movieId",
+                    outputColumnName: "movieIdEncoded"));
+
+
+            var trainingDataEstimator = pipeline.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+            Console.WriteLine("Training the model...");
+            var model = trainingDataEstimator.Fit(trainingDataView);
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Evaluating the model ===============");
+            var prediction = model.Transform(testDataView);
+            var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+
+            Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
+            Console.WriteLine("RSquared: " + metrics.LossFunction.ToString());
+            Console.WriteLine("RSquared: " + metrics.MeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.MeanAbsoluteError.ToString());
+            Console.WriteLine();
+
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<MovieRatingModel, MovieRatingPredictionModel>(model);
+            Console.WriteLine("Calculating the top 5 movies for user 611...");
+            var top5 = (from m in All
+                        let p = predictionEngine.Predict(
+                           new MovieRatingModel()
+                           {
+                               userId = 611,
+                               movieId = m.Id
+                           })
+                        orderby p.Score descending
+                        select (MovieId: m.Id, Score: p.Score)).Take(5);
+
+            foreach (var t in top5)
+                Console.WriteLine($"  Score:{t.Score}\tMovie: {Dohvati(t.MovieId)?.Title}");
+
+
+
         }
 
-        public async Task<List<MoviesModel>> GetAllMovies()
+        public static MovieModel Dohvati(int id)
         {
-            var client = InitializeClient();
-            var database = client.GetDatabase("sample_mflix");
-            var collection = database.GetCollection<MoviesModel>("movies");
-            var response = collection.Find(movie => movie.Year >= 2005).ToListAsync();
-
-            return await response;
-        }
-
-
-        public async Task<List<MoviesModel>> GetSpecificMovie(string name)
-        {
-            var client = InitializeClient();
-            var database = client.GetDatabase("sample_mflix");
-            var collection = database.GetCollection<MoviesModel>("movies");
-            var response = collection.Find(movie => movie.Title.ToLower() == name.ToLower()).ToListAsync();
-
-            return await response;
+            return All.Single(m => m.Id == id);
         }
     }
 }
+
